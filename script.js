84c2932768990790eaae8fc13f949c65c051b414
Vladyslav Zachepa <112680328+zachepka@users.noreply.github.com>
2024-12-23 02:21:42 -0330

    changes. Dec23

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let inputString = prompt("Введіть рядок:");
-let charsToRemoveString = prompt("Введіть символи для видалення:");
-
-if (inputString === null || inputString.trim() === '' || charsToRemoveString === null || charsToRemoveString.trim() === '') {
-    console.log("Error!");
-} else {
-    let result = '';
-    for (let char of inputString) {
-        if (!charsToRemoveString.includes(char)) {
-            result += char;
+function removeChars(inputString, charsToRemove) {
+    if (inputString?.trim() || charsToRemove === null) {
+      return "Error!";
+    } else {
+      let result = '';
+      for (const char of inputString) {
+        if (!charsToRemove.includes(char)) {
+          result += char;
         }
+      }
+      return result;
     }
-
+  }
+  
+  let inputString = prompt("Введіть рядок:");
+  let charsToRemoveString = prompt("Введіть символи для видалення:");
+  
+  if (inputString !== null && charsToRemoveString !== null) {
+    let charsToRemove = charsToRemoveString.split('');
+    let result = removeChars(inputString, charsToRemove);
     console.log(result);
-}
+  }
